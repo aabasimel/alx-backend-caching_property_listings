6a8646433674cc0e184a38550e314712a08dcbbe
Ahmed <abasimelahmed@gmail.com>
2025-11-02 18:01:43 +0100

    Updated

--- properties/utils.py
+++ properties/utils.py
@@ -84,7 +84,7 @@ def get_redis_cache_metrics():
         if total_requests > 0:
             hit_ratio = (hits / total_requests) * 100
         else:
-            hit_ratio = 0.0
+            hit_ratio = 0
 
         metrics={
             "hits":hits,
