d368662251be56e91cd1c4474974413515884796
Ahmed <abasimelahmed@gmail.com>
2025-11-02 16:24:58 +0100

    Updated

--- properties/views.py
+++ properties/views.py
@@ -1,12 +1,20 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 from .utils import get_all_properties
-from .models import Property
 
 
-@cache_page(60*15)
+@cache_page(60 * 15)  # Cache view response for 15 minutes
 def property_list(request):
-    property=Property.objects.all().value(
-       'id', 'title', 'description', 'price', 'location', 'created_at' 
-    )
-    return JsonResponse(list(property),safe=False)
+    """
+    View to return all properties.
+    Uses low-level cache API to cache queryset for 1 hour.
+    View response is also cached for 15 minutes.
+    """
+    response = get_all_properties()
+
+    properties = response.get('data', [])
+
+    return JsonResponse({
+        'count': len(properties),
+        'data': properties
+    }, safe=False)
