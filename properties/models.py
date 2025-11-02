204b2c3f5111371761ab2a5a76074ec9419a3bed
Ahmed <abasimelahmed@gmail.com>
2025-11-02 15:52:12 +0100

    Updated

--- properties/models.py
+++ properties/models.py
@@ -1,11 +1,14 @@
+import uuid
+
 from django.db import models
 
 class Property(models.Model):
-    title=models.CharField(max_length=200)
-    description=models.TextField()
-    prince=models.DecimalField(decimal_places=2,max_digits=10)
-    location=models.CharField(max_length=100)
-    created_at=models.DateTimeField(auto_now_add=True)
+    property_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+from .utils import get_all_properties
+from .models import Property
 
-# Create your views here.
+
+@cache_page(60*15)
+def property_list(request):
+    property=Property.objects.all().value(
+       'id', 'title', 'description', 'price', 'location', 'created_at' 
+    )
+    return JsonResponse(list(property),safe=False)
